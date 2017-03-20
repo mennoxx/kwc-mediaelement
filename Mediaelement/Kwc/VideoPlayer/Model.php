dc328c609eeeab062f0800a2f0d8b74c0ecaae8d
Michael Tiefenthaler <mt@vivid-planet.com>
2017-03-20 15:47:29 +0100

    Add videoplayer-component

---
+++ Mediaelement/Kwc/VideoPlayer/Model.php
@@ -0,0 +1,20 @@
+<?php
+class Mediaelement_Kwc_VideoPlayer_Model extends Kwf_Model_Db_Proxy
+{
+    protected $_table = 'kwc_advanced_video_player';
+
+    protected $_referenceMap    = array(
+        'FileMp4' => array(
+            'column'           => 'mp4_kwf_upload_id',
+            'refModelClass'     => 'Kwf_Uploads_Model',
+        ),
+        'FileWebm' => array(
+            'column'           => 'webm_kwf_upload_id',
+            'refModelClass'     => 'Kwf_Uploads_Model',
+        ),
+        'FileOgg' => array(
+            'column'           => 'ogg_kwf_upload_id',
+            'refModelClass'     => 'Kwf_Uploads_Model',
+        )
+    );
+}
